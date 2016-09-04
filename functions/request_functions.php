304df2be80c1a1c2868fb272b710653cd3d30d9c
DESKTOP-KENJUVM\Nick <nick.bensein@gmail.com>
2016-09-04 23:13:34 +1000

    backend for request form

---
+++ functions/request_functions.php
@@ -0,0 +1,58 @@
+<?php
+function registerRequest()
+{ 
+    if (session_id() == '')
+    {
+        session_start();
+    }
+    if(isset($_SESSION['email']))
+    {       
+        if (isset($_POST))
+                {
+                     if (!empty($_POST) && !empty($_POST['pickUp']))
+                        {
+
+                             $pickUp = $_POST['pickUp'];
+                             $dropOff = $_POST['dropOff'];
+                             $recipient = $_POST['recipient'];
+                             $premium = $_POST['premium'];
+                             $fragile = $_POST['fragile'];
+                             $special = $_POST['special'];
+                             $email = $_SESSION['email'];
+
+                             $a = date_parse_from_format('Y-m-d', $_POST['pickDate']);
+                             $pickTime = date_parse_from_format('H:i', $_POST['pickTime']);
+
+                             $pickDate = mktime($pickTime['hour'], $pickTime['minute'], 0, $a['month'], $a['day'], $a['year']);
+                             $b = date_parse_from_format('Y-m-d', $_POST['dropDate']);
+                             $dropTime = date_parse_from_format('H:i', $_POST['dropTime']);
+
+                             $dropDate = mktime($dropTime['hour'], $dropTime['minute'], 0, $b['month'], $b['day'], $b['year']);
+                             try
+                             {
+                                 echo "stuff";
+                                 $pdo = connect();
+                                 $query= "INSERT INTO delivery(origin, destination, user, driver, name, pickup, dropoff, type, fragile, special)
+                                 VALUES(:pickUp, :dropOff, :user, 'dad', :recipient, :pickTime, :dropTime, :first, :fragile, :special);";
+                                 $prepare = $pdo->prepare($query);
+                                 $prepare -> bindValue(':pickUp', $pickUp);
+                                 $prepare -> bindValue(':pickTime', $pickDate);
+                                 $prepare -> bindValue(':dropOff', $dropOff);
+                                 $prepare -> bindValue(':dropTime', $dropDate);
+                                 $prepare -> bindValue(':recipient', $recipient);
+                                 $prepare -> bindValue(':first', $premium);
+                                 $prepare -> bindValue(':fragile', $fragile);  
+                                 $prepare -> bindValue(':special', $special);
+                                 $prepare -> bindValue(':user', $email);
+                                 $prepare -> execute();
+                             }
+                             catch (PDOException $e)
+                             {
+                                 echo $e -> getMessage();
+                             }
+                         print_r($_POST);
+                     }
+                }
+            }
+}
+?>
