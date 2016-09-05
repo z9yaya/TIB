bf2d46e33f07cff47c519ef0dd04d557c607dfca
MichaelPhong <m.phong@hotmail.com>
2016-09-05 16:14:09 +1000

    Removed weird merging glitch

--- pages/history.php
+++ pages/history.php
@@ -77,41 +77,6 @@
     </table>
      <?php $conn->close(); ?>
 					
->>>>>>> 6223e82d20b90fbfce485141534e58bc4261b863
-
-      //execute the SQL query and return records
-	  $sql = "SELECT delivery_id, time, location FROM history";
-	  $result = $conn->query($sql);
-      ?>
-	  <h1>Package History</h1>
-      <table border="2" style= "background-color: #84ed86; color: black; margin: 0 auto;" >
-      <thead>
-        <tr>
-          <th>Delivery ID</th>
-          <th>Time Arrived</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-		if ($result->num_rows > 0) 
-		{
-          while( $row = $result->fetch_assoc()){
-            echo
-            "<tr>
-              <td>$row[delivery_id]</td>
-			  <td>$row[time]</td>
-			  <td>$row[location]</td>
-            </tr>\n";
-          }
-		}
-		else {
-				echo "No Results Found";} 
-        ?>
-      </tbody>
-    </table>
-    <?php $conn->close(); ?>
-	
 	<br>
 	<form method="get" action="complaints.php">
 		<button type="submit">Report an Issue</button>
