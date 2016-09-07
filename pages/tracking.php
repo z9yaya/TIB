a17773e4066f9d0d21decf3983a78cffee5deb36
QUT15 <eliasrihan@yahoo.com>
2016-09-07 22:57:07 +1000

    tracking.php wasn't graping the data package so updating it again

--- pages/tracking.php
+++ pages/tracking.php
@@ -32,9 +32,21 @@
 				</header>
 					<div id="content">  
 				<?php include '../functions/functions.php';
+  $servername = "localhost";
+  $username = "edit";
+  $password = "editme";
+  $dbname = "tib";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  
+  if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);}	  
+
+      //execute the SQL query and return records
 	  //$results = GrabData('history', 'delivery_ID, time, location', 'delivery_ID', 1);
-	  $sql = "SELECT delivery_id, weight, content FROM package";
-	  //$results = $conn->query($sql);
+	  $sql = "SELECT ID, delivery_id, weight, content FROM package";
+	  $results = $conn->query($sql);
+      ?>	   
       ?>	   
 				
 	    
