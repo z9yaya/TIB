2afbbeaae76159abd4aecc7eb569130449362467
MichaelPhong <m.phong@hotmail.com>
2016-09-07 22:52:27 +1000

    Created the basic deliveries table

--- pages/deliveries.php
+++ pages/deliveries.php
@@ -29,10 +29,9 @@
                                                                             echo 'SIGN IN</a>';}?>
 					
 					<a id="header" class="intro intro_blue" href="../index.php">drop.it</a>
-					<a id="deliveries" class="menu menu_blue" href="deliveries.php">DELIVERIES</a>
+					<a id="deliveries" class="menu menu_blue selected" href="deliveries.php">DELIVERIES</a>
 					<a id="tracking" class="menu menu_blue selected" href="tracking.php">TRACKING</a>
                     <a id="request" class="menu menu_blue" href="request.php">REQUEST</a>
-					<a id="request" class="menu menu_blue" href="history.php">HISTORY</a>
 				</header>
 					<div id="content">
 					
@@ -46,13 +45,71 @@
 	  
 	  if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);}	  
+	  ?>
+	  
+	  <?php
+	  $delivery = "SELECT * FROM delivery";
+	  $deliveryresult = $conn->query($delivery);	  
+	  ?>	  
+	  <h1>Deliveries</h1>
+      <table border="2" style= "background-color: #84ed86; color: black; margin: 0 auto;" >
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>User</th>
+          <th>Driver</th>
+		  <th>Origin</th>
+		  <th>Destination</th>
+		  <th>Name</th>
+		  <th>Pick Up</th>
+		  <th>Drop Off</th>
+		  <th>Cost</th>
+		  <th>Type</th>
+		  <th>Paid</th>
+		  <th>Date Paid</th>
+		  <th>Fragile</th>
+		  <th>Special</th>
+		  <th>Status</th>		  
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+		if ($deliveryresult->num_rows > 0) 
+		{
+          while( $row = $deliveryresult->fetch_assoc()){
+            echo
+            "<tr>
+              <td>$row[ID]</td>
+			  <td>$row[user]</td>
+			  <td>$row[driver]</td>
+			  <td>$row[origin]</td>
+			  <td>$row[destination]</td>
+			  <td>$row[name]</td>
+			  <td>$row[pickup]</td>
+			  <td>$row[dropoff]</td>
+			  <td>$row[cost]</td>
+			  <td>$row[type]</td>
+			  <td>$row[paid]</td>
+			  <td>" . date('h:i:s\ d-m-Y',$row[date_paid]) . "</td>
+			  <td>$row[fragile]</td>
+			  <td>$row[special]</td>
+			  <td>$row[status]</td>
+			  <td><a href='complaints.php?id=".$row['ID']."'>Report an Issue</a></td>
+            </tr>\n";
+          }
+		}
+		else {
+				echo "No Results Found";} 
+        ?>
+      </tbody>
+    </table>	  
+	  
+	  
 
+	  <?php
       //execute the SQL query and return records
 	  $sql = "SELECT delivery_id, time, location FROM history";
 	  $result = $conn->query($sql);
-	  $time = "SELECT time FROM history";
-	  $timeresult = $conn->query($time); 
-	  $timestamp = date("Y-m-d\H:i:s", $timeresult)
       ?>
 	  <h1>Package History</h1>
       <table border="2" style= "background-color: #84ed86; color: black; margin: 0 auto;" >
