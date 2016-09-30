dd555d680543df8950e4b485fa7685e73884bdbd
z9yaya <ze_yaya@msn.com>
2016-09-30 15:52:48 +1000

    Removed outdate code

--- AJAX/createChat.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $post_data = $_POST['data'];
-if (!empty($post_data)) {
-    $filename = $post_data;
-    $handle = fopen($filename, "a");
-    fwrite($handle, '');
-    fclose($handle);
-}
-?>
